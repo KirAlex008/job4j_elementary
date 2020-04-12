08af3e8be605b2a65868ba477b8353a044ff83fb
KirAlex008 <59695224+KirAlex008@users.noreply.github.com>
2020-04-12 18:42:46 +0300

    1. Модульные тесты.[#269774]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -6,13 +6,13 @@ public class Converter {
         return rsl;
     }
     public static int rubleToDollar(int value) {
-        int rsl = value / 60;
-        return rsl;
+        // int rsl = value / 60;
+        return -1; //rsl;
     }
     public static void main(String[] args) {
         int euro = Converter.rubleToEuro(140);
         System.out.println("140 rubles are " + euro + " euro.");
-        int dollar = Converter.rubleToDollar(180);
-        System.out.println("140 rubles are " + dollar + " dollars.");
+        //int dollar = Converter.rubleToDollar(180);
+        //System.out.println("140 rubles are " + dollar + " dollars.");
     }
 }
