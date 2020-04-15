265294af3333484fc5e3533a46ba491d6bb234af
KirAlex008 <59695224+KirAlex008@users.noreply.github.com>
2020-04-15 13:02:30 +0300

    4.4. Максимум из трех чисел[#269687]

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -4,8 +4,8 @@ public class SqArea {
     public static double square(int p, int k) {
         double h = p / (2 * (k + 1));
         double l = h * k;
-        double rsl = l * h;
-        return rsl;
+        double s = l * h;
+        return s;
     }
 
     public static void main(String[] args) {
--- test/ru/job4j/condition/SqAreaTest.java
+++ test/ru/job4j/condition/SqAreaTest.java
@@ -1,16 +1,27 @@
 package ru.job4j.condition;
 
 import org.junit.Test;
-import org.junit.Assert;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
 
 public class SqAreaTest {
 
     @Test
-    public void whenP6andK2ThenSquare2() {
-        int in1 = 6;
-        int in2 = 2;
-        int expected = 2;
-        double out = SqArea.square(6, 2);
-        Assert.assertEquals(expected, out, 0.01);
+    public void whenp4k1s1() {
+        int k = 1;
+        int p = 4;
+        double expected = 1;
+        double s = SqArea.square(p, k);
+        assertThat(s, is(expected));
+    }
+
+    @Test
+    public void whenp6k2s2() {
+        int k = 2;
+        int p = 6;
+        double expected = 2;
+        double s = SqArea.square(p, k);
+        assertThat(s, is(expected));
     }
 }
