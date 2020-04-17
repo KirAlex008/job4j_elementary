809f2d69f8c2b177e5a9d01b86db0007c401babc
KirAlex008 <59695224+KirAlex008@users.noreply.github.com>
2020-04-17 18:56:26 +0300

    5.8. JavaDoc. Code convention[#269769]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -1,18 +1,38 @@
 package ru.job4j.converter;
 
+/**
+ * Class Converter решение задачи Модульные тесты.[#269774]
+ * @autor kirsha
+ * @since 17.04.2020
+ * @version 1
+ */
 public class Converter {
+    /**
+     * rubelToEuro
+     * @param value - сумма в рублях
+     * @return rsl - сумма в евро
+     */
     public static int rubleToEuro(int value) {
         int rsl = value / 70;
         return rsl;
     }
+    /**
+     * rubelToDollar
+     * @param value - сумма в рублях
+     * @return rsl - сумма в доллорах
+     */
     public static int rubleToDollar(int value) {
-        // int rsl = value / 60;
-        return -1; //rsl;
+        int rsl = value / 60;
+        return rsl;
     }
+    /**
+     * main
+     * @param args - args
+     */
     public static void main(String[] args) {
         int euro = Converter.rubleToEuro(140);
         System.out.println("140 rubles are " + euro + " euro.");
-        //int dollar = Converter.rubleToDollar(180);
-        //System.out.println("140 rubles are " + dollar + " dollars.");
+        int dollar = Converter.rubleToDollar(180);
+        System.out.println("140 rubles are " + dollar + " dollars.");
     }
 }
