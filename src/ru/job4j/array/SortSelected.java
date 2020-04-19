63c982fc66d7d1f95e1b3b9b905ff65dbd9140fa
KirAlex008 <59695224+KirAlex008@users.noreply.github.com>
2020-04-19 20:52:24 +0300

    6.5.2. Поиск минимума в диапазоне.[#269702]

--- src/ru/job4j/array/SortSelected.java
+++ src/ru/job4j/array/SortSelected.java
@@ -6,7 +6,7 @@ public class SortSelected {
         //int min = MinDiapason.findMin(data, 0, data.length);
         //int index = FindLoop.indexOf(data, min, 0, data.length);
         for (int i = 0; i < data.length; i++) {
-            int min = MinDiapason.findMin(data, i, data.length);
+            int min = MinDiapason.findMin(data, i, data.length - 1);
             int index = FindLoop.indexOf(data, min, i, data.length);
             int temp = data[i];
             data[i] = min;
