e49ca7af782edf7e6e7a6ebd84e518ea217f1fcf
KirAlex008 <59695224+KirAlex008@users.noreply.github.com>
2020-04-22 14:18:13 +0300

    6.7.4. Выигрышные комбинации в сокобан[#269696] исправление ошибки

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -31,7 +31,7 @@ public class MatrixCheck {
     public static boolean isWin(char[][] board) {
         boolean result = false;
         char[] diag = MatrixCheck.extractDiagonal(board);
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < board.length; i++) {
             if ((diag[i] == 'X') && ((monoHorizontal(board, i) || monoVertical(board, i)))) {
                 result = true;
                 break;
