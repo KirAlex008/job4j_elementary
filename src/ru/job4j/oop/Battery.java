2dc93093527b8c8ad9880a97433410d571a34a18
KirAlex008 <59695224+KirAlex008@users.noreply.github.com>
2020-04-25 20:50:00 +0300

    1.7. Состояние объекта.[#269860]

--- src/ru/job4j/oop/Battery.java
+++ src/ru/job4j/oop/Battery.java
@@ -7,12 +7,12 @@ public class Battery {
         this.load = size;
     }
     public void exchange(Battery another) {
-        this.load = this.load + another.load;
-        another.load = 0;
+        this.load = this.load - another.load;
+        another.load = another.load * 2;
     }
     public static void main(String[] args) {
-        Battery battery1 = new Battery(20);
-        Battery battery2 = new Battery(100);
+        Battery battery1 = new Battery(100);
+        Battery battery2 = new Battery(60);
         battery1.exchange(battery2);
         System.out.println("Battery 1 has: " + battery1.load);
         System.out.println("Battery 2 has: " + battery2.load);
