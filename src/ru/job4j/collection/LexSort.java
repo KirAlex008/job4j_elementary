b8e6b22369e7d870627766ead9dbe43be7584560
KirAlex008 <59695224+KirAlex008@users.noreply.github.com>
2020-05-22 12:52:27 +0300

    4. Сортировка номера [#269789] исправление ошибки

--- src/ru/job4j/collection/LexSort.java
+++ src/ru/job4j/collection/LexSort.java
@@ -8,19 +8,8 @@ public class LexSort implements Comparator<String> {
 
     @Override
     public int compare(String left, String right) {
-        List<String> list = List.of("1", "2", "3", "4", "5", "6", "7", "9", "0");
-        String lineLeft = "";
-        String lineRight = "";
-        for (int i = 0; i <= left.length() - 1; i++) {
-            if (list.contains("" + left.charAt(i))) {
-                lineLeft = lineLeft + left.charAt(i);
-            }
-        }
-        for (int i = 0; i <= right.length() - 1; i++) {
-            if (list.contains("" + right.charAt(i))) {
-                lineRight = lineRight + right.charAt(i);
-            }
-        }
-        return Integer.compare(Integer.valueOf(lineLeft), Integer.valueOf(lineRight));
+        String[] leftNummer = left.split("\\.");
+        String[] rightNummer = right.split("\\.");
+        return Integer.compare(Integer.valueOf(leftNummer[0]), Integer.valueOf(rightNummer[0]));
     }
 }
